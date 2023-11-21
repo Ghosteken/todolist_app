9c7744d9afe82aeffa3668d15f67ac85fdb22f52
Nicholas <aigberuan6@gmail.com>
2023-11-21 01:20:21 +0100

    id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import { TodoItem } from "@/componets/Tdoditem"
 import prisma from "@/db"
 import Link from "next/link"
+function getTodos() {
+  return prisma.todo.findMany()
+}
 
 
 
 export default async function Home() {
 
-  const todos= await prisma.todo.findMany()
-  await prisma.todo.create({data: {title: "test", complete: false}})
+  const todos= await getTodos()
+  //await prisma.todo.create({data: {title: "test", complete: false}})
   return <>
     <header className="flex justify-between
     items-center mb-4 " >
@@ -14,9 +18,7 @@ export default async function Home() {
       <Link className="border border slate-300 text slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none " href="/new">new</Link>
     </header>
     <ul className="pl-4" >{todos.map(todo =>(
-      <li key={todo.id}>{todo.title}
-        
-      </li>
+      <TodoItem key={todo.id} {...todo}  />
     ))} </ul>
   </>
 }
