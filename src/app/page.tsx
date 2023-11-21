78d8df70017b98cb8dbdbd8d5f38f2a040c48467
Nicholas <aigberuan6@gmail.com>
2023-11-21 02:29:55 +0100

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import { TodoItem } from "@/componets/Tdoditem"
 import prisma from "@/db"
+import { log } from "console"
 import Link from "next/link"
 function getTodos() {
   return prisma.todo.findMany()
 }
 
+async function toggleTodo(id: string,complete: boolean) {
+  "use server"
+
+  await prisma.todo.update({
+    where: { id },
+    data: { complete },
+  })
+  
+}
 
 
 export default async function Home() {
@@ -18,7 +28,7 @@ export default async function Home() {
       <   Link className="border border slate-300 text slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none " href="/new">new</Link>
     </header>
     <ul className="pl-4" >{todos.map(todo => (
-      <TodoItem key={todo.id} {...todo} />
+      <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
     ))} </ul>
   </>
 }
