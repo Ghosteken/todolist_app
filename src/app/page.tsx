8607bc716953344bf6f8c970f837b7d7aad4c1e8
Nicholas <aigberuan6@gmail.com>
2023-11-21 01:34:24 +0100

    again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,16 @@ function getTodos() {
 
 export default async function Home() {
 
-  const todos= await getTodos()
+  const todos = await getTodos()
   //await prisma.todo.create({data: {title: "test", complete: false}})
   return <>
     <header className="flex justify-between
     items-center mb-4 " >
       <h1 className="text-2xl" >Todos</h1>
-      <Link className="border border slate-300 text slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none " href="/new">new</Link>
+      <   Link className="border border slate-300 text slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none " href="/new">new</Link>
     </header>
-    <ul className="pl-4" >{todos.map(todo =>(
-      <TodoItem key={todo.id} {...todo}  />
+    <ul className="pl-4" >{todos.map(todo => (
+      <TodoItem key={todo.id} {...todo} />
     ))} </ul>
   </>
 }
